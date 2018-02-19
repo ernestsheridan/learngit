5b3b0f438d688e05793cfc17e215e3ebb525e501
Ernest Sheridan <Ernest@Airys-MacBook-Pro.local>
2018-02-19 18:13:18 +0700

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Ernest Sheridan on 2/19/18.
+//  Copyright © 2018 Ernest Sheridan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
